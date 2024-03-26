dc07663bbaea0ce58e3a067695faf2d7d4d4ae14
amir soltani <amirsoltani686@gmail.com>
2024-03-26 17:31:29 +0330

    Initializes the project

---
+++ Movies/Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Amir on 3/26/24.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
