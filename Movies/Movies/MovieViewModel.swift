2f610a6528629d4af6d88852958daf49c11ea759
amir soltani <amirsoltani686@gmail.com>
2024-03-26 17:46:02 +0330

    define: Creates a movie view model to hold the state of the current movie

---
+++ Movies/Movies/MovieViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  MovieViewModel.swift
+//  Movies
+//
+//  Created by Amir on 3/26/24.
+//
+
+import Foundation
+
+class MovieViewModel {
+    let id: UUID
+    let title: String
+    let year: String
+    let genre: String
+    let description: String
+    let thumbURL: String
+    
+    init(movie: Movie) {
+        self.id = movie.id ?? UUID()
+        self.title = movie.title ?? ""
+        self.year = movie.year ?? ""
+        self.genre = movie.genre ?? ""
+        self.description = movie.description ?? ""
+        self.thumbURL = movie.thumbURL ?? ""
+    }
+}
