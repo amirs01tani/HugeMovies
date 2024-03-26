53c8cdb062ec25d9e04937dedc1f6ffa53cec78d
amir soltani <amirsoltani686@gmail.com>
2024-03-26 17:43:46 +0330

    define: Creates a struct for movie entity with optional properties in order to accept null values if happens

---
+++ Movies/Movies/Movie.swift
@@ -0,0 +1,17 @@
+//
+//  Movie.swift
+//  Movies
+//
+//  Created by Amir on 3/26/24.
+//
+
+import Foundation
+
+struct Movie {
+    let id: UUID?
+    let title: String?
+    let year: String?
+    let genre: String?
+    let description: String?
+    let thumbURL: String?
+}
